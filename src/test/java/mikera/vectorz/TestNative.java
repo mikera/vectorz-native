eb9d7a1590c52713681bdd37fba125bbeaf970e0
Mike <mike@mikera.net>
2015-06-18 17:53:38 +0100

    Add dependencies, JUnit testing

---
+++ src/test/java/mikera/vectorz/TestNative.java
@@ -0,0 +1,13 @@
+package mikera.vectorz;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestNative {
+	
+	@Test public void testTest() {
+		assertEquals(1,1);
+	}
+
+}
