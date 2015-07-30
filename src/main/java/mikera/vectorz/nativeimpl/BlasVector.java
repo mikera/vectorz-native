59883c9d45caa2a4c052d88ff9bd99a4d0a80f33
mikera <mike.r.anderson.13@gmail.com>
2015-07-30 13:07:10 +0800

    Add BlasVector with dependency on new BaseStridedVector in vectorz snapshot

---
+++ src/main/java/mikera/vectorz/nativeimpl/BlasVector.java
@@ -0,0 +1,31 @@
+package mikera.vectorz.nativeimpl;
+
+import mikera.vectorz.AVector;
+import mikera.vectorz.impl.AStridedVector;
+import mikera.vectorz.impl.BaseStridedVector;
+
+public class BlasVector extends BaseStridedVector {
+
+	protected BlasVector(int length, double[] data, int offset, int stride) {
+		super(length, data,offset,stride);
+	}
+	
+	public static BlasVector wrap(double[] data) {
+		return new BlasVector(data.length,data,0,1);
+	}
+
+	@Override
+	public int getArrayOffset() {
+		return offset;
+	}
+
+	@Override
+	public int getStride() {
+		return stride;
+	}
+
+	@Override
+	public AVector exactClone() {
+		return new BlasVector(length,data.clone(),offset,stride);
+	}
+}
--- src/test/java/mikera/vectorz/TestNative.java
+++ src/test/java/mikera/vectorz/TestNative.java
@@ -1,6 +1,9 @@
 package mikera.vectorz;
 
 import static org.junit.Assert.*;
+
+import mikera.vectorz.impl.ArraySubVector;
+import mikera.vectorz.nativeimpl.BlasVector;
 import mikera.vectorz.nativeimpl.NativeUtil;
 
 import org.junit.Test;
@@ -9,6 +12,21 @@ public class TestNative {
 	
 	@Test public void testBuffer() {
 		assertEquals(9,NativeUtil.createBuffer(9).capacity());
+	}
+	
+	public void doGenericTests(AVector a) {
+		new mikera.vectorz.TestVectors ().doGenericTests(a);
+	}
+	
+	@Test public void g_BlasVector() {
+		double[] data=new double[100];
+		for (int i=0; i<100; i++) {
+			data[i]=i+(1.0/Math.PI);
+		}
+		
+		BlasVector v=BlasVector.wrap(data);
+		
+		doGenericTests(v);
 	}
 
 }
