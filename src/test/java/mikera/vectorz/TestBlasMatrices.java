b9e12a05ab383f63d0448d90c822d36dddee4639
mikera <mike.r.anderson.13@gmail.com>
2015-07-30 16:09:31 +0800

    add square matrix for testing

--- src/test/java/mikera/vectorz/TestBlasMatrices.java
+++ src/test/java/mikera/vectorz/TestBlasMatrices.java
@@ -12,7 +12,7 @@ import mikera.vectorz.nativeimpl.BlasVector;
 import org.junit.Test;
 
 public class TestBlasMatrices extends TestNative {
-
+	
 	public void doGenericTests(AMatrix m) {
 		new mikera.matrixx.TestMatrices().doGenericTests(m);
 	}
@@ -28,5 +28,15 @@ public class TestBlasMatrices extends TestNative {
 		
 		BlasMatrix m2=BlasMatrix.wrap(data,6,5);
 		doGenericTests(m2);
+	}
+	
+	@Test public void g_SquareMatrix() {
+		double[] data=new double[16];
+		for (int i=0; i<16; i++) {
+			data[i]=i+(1.0/Math.PI);
+		}
+		
+		BlasMatrix m1=BlasMatrix.wrap(data,4,4);
+		doGenericTests(m1);
 	}
 }
